5bed4f50aa827019e41aae81a4bc13fbeaf7ef9b
Ankit Jain <ankitja@nodeb11.englab.juniper.net>
2014-04-21 23:45:01 -0700

    changing mailing-list

--- fabfile/testbeds/testbed_havana_ubuntu_1_node_webui.py
+++ fabfile/testbeds/testbed_havana_ubuntu_1_node_webui.py
@@ -5,12 +5,12 @@ os_password = 'contrail123'
 os_tenant_name = 'demo'
 multi_tenancy = False
 webui = True
-host1='root@10.204.217.53'
+host1='root@10.204.217.64'
 ext_routers = []
 router_asn = 64512
 public_vn_rtgt = 10000
 public_vn_subnet = "10.84.41.0/24"
-host_build = 'ankitja@10.204.216.49'
+host_build = 'stack@10.204.216.49'
 env.roledefs = {
     'all': [host1],
     'cfgm': [host1],
@@ -23,7 +23,7 @@ env.roledefs = {
     'build': [host_build],
 }
 env.hostnames = {
-    'all': ['nodeg13']
+    'all': ['nodeg24']
 }
 env.ostypes = {
      host1: 'ubuntu'
@@ -36,5 +36,5 @@ env.passwords = {
 
 env.test_repo_dir='/home/stack/webui_ubuntu_single_node/havana/contrail-test'
 env.mail_from='ankitja@juniper.net'
-env.mail_to='ankitja@juniper.net'
-
+env.mail_to='dl-contrail-sw@juniper.net'
+env.log_scenario='Ubuntu-Havana Single-Node Webui Sanity'
