607c7bbeef4f8b604162759a9aea182313eaaec1
Ignatious Johnson <ijohnson@juniper.net>
2014-08-29 20:33:55 -0700

    Checking for correct status string in the zookeepr status output during zookeeper rolling restart. Addding more debug statements in zookeeper.py for debugging and replace using zkServer.sh instead of service command. Change-Id: I3c8661bb8edc5b81389fcd275a3ac4521b56bae7

--- fabfile/tasks/zookeeper.py
+++ fabfile/tasks/zookeeper.py
@@ -23,8 +23,15 @@ def zookeeper_rolling_restart():
     if cfgm_nodes == database_nodes:
         print "No need for rolling restart."
 
+    stop_cmd = "/usr/lib/zookeeper/bin/zkServer.sh stop"
+    restart_cmd = "/usr/share/zookeeper/bin/zkServer.sh restart"
+    if detect_ostype() in ['Ubuntu']:
+        stop_cmd = "/usr/share/zookeeper/bin/zkServer.sh stop"
+        restart_cmd = "/usr/share/zookeeper/bin/zkServer.sh restart"
+
     if (len(database_nodes) > 1 and
         'leader' in zookeeper_status.values() and
+        'follower' in zookeeper_status.values() and
         'notrunning' not in zookeeper_status.values() and
         'notinstalled' not in zookeeper_status.values() and
         'standalone' not in zookeeper_status.values()):
@@ -32,7 +39,7 @@ def zookeeper_rolling_restart():
         print "Zookeeper quorum is already formed properly."
         return
     elif (len(database_nodes) == 1 and
-        'No such file' not in zookeeper_status.values() and
+        'notinstalled' not in zookeeper_status.values() and
         'standalone' in zookeeper_status.values()):
         print zookeeper_status
         print "Zookeeper quorum is already formed properly."
@@ -45,23 +52,23 @@ def zookeeper_rolling_restart():
         zk_index = (database_nodes.index(new_node) + len(cfgm_nodes) + 1)
         with settings(host_string=new_node, password=env.passwords[new_node]):
             pdist = detect_ostype()
-            # Install zookeeper in the new node.
+            print "Install zookeeper in the new node."
             execute('create_install_repo_node', new_node)
             upgrade_package(['python-contrail', 'contrail-openstack-database', 'zookeeper'], pdist)
             if pdist in ['Ubuntu']:
                 run("ln -sf /bin/true /sbin/chkconfig")
             run("chkconfig zookeeper on")
-            # Fix zookeeper configs
+            print "Fix zookeeper configs"
             run("sudo sed 's/^#log4j.appender.ROLLINGFILE.MaxBackupIndex=/log4j.appender.ROLLINGFILE.MaxBackupIndex=/g' /etc/zookeeper/conf/log4j.properties > log4j.properties.new")
             run("sudo mv log4j.properties.new /etc/zookeeper/conf/log4j.properties")
             if pdist in ['centos']:
                 run('echo export ZOO_LOG4J_PROP="INFO,CONSOLE,ROLLINGFILE" >> /usr/lib/zookeeper/bin/zkEnv.sh')
             if pdist in ['Ubuntu']:
                 run('echo ZOO_LOG4J_PROP="INFO,CONSOLE,ROLLINGFILE" >> /etc/zookeeper/conf/environment')
-            #put cluster-unique zookeeper's instance id in myid
+            print "put cluster-unique zookeeper's instance id in myid"
             run('sudo echo "%s" > /var/lib/zookeeper/myid' % (zk_index))
 
-    # Add new nodes to existing zookeeper quorum
+    print "Add new nodes to existing zookeeper quorum"
     with settings(host_string=cfgm_nodes[0], password=env.passwords[cfgm_nodes[0]]):
         for new_node in new_nodes:
             zk_index = (database_nodes.index(new_node) + len(cfgm_nodes) + 1)
@@ -69,40 +76,39 @@ def zookeeper_rolling_restart():
         tmp_dir= tempfile.mkdtemp()
         get(zoo_cfg, tmp_dir)
 
-    # Restart zookeeper in all nodes to make new nodes join zookeeper quorum
+    print "Restart zookeeper in all nodes to make new nodes join zookeeper quorum"
     for zookeeper_node in cfgm_nodes + new_nodes:
         with settings(host_string=zookeeper_node, password=env.passwords[zookeeper_node]):
             put(tmp_dir+'/zoo.cfg', zoo_cfg)
-            restart_cmd = "/usr/lib/zookeeper/bin/zkServer.sh restart"
-            if detect_ostype() in ['Ubuntu']:
-                restart_cmd = "/usr/share/zookeeper/bin/zkServer.sh restart"
-            # Start Zookeeper in new database node
+            print "Start Zookeeper in new database node"
             run(restart_cmd)
 
     print "Waiting 5 seconds for the new nodes in the zookeeper quorum to be synced."
     sleep(5)
-    # Shutdown onld nodes one by one and also make sure leader/follower election is complete.
-    # after each shut downs
+    print "Shutdown old nodes one by one and also make sure leader/follower election is complete after each shut downs"
     zoo_nodes = cfgm_nodes + database_nodes
     for old_node in old_nodes:
         zoo_nodes.remove(old_node)
         with settings(host_string=old_node, password=env.passwords[old_node]):
-            # Stop Zookeeper in old cfgm node
-            run("service zookeeper stop")
+            print "Stop Zookeeper in old cfgm node"
+            run(stop_cmd)
             retries = 3
             while retries:
                 zookeeper_status = verfiy_zookeeper(*zoo_nodes)
                 if (len(zoo_nodes) > 1 and
                     'leader' in zookeeper_status.values() and
-                    'not running' in zookeeper_status.values() and
+                    'follower' in zookeeper_status.values() and
+                    'notrunning' not in zookeeper_status.values() and
+                    'notinstalled' not in zookeeper_status.values() and
                     'standalone' not in zookeeper_status.values()):
                     print zookeeper_status
-                    print "Zookeeper quorum is already formed properly."
+                    print "Zookeeper quorum is formed properly."
                     break
                 elif (len(zoo_nodes) == 1 and
+                    'notinstalled' not in zookeeper_status.values() and
                     'standalone' in zookeeper_status.values()):
                     print zookeeper_status
-                    print "Zookeeper quorum is already formed properly."
+                    print "Zookeeper quorum is formed properly."
                     break
                 else:
                     retries -= 1
@@ -112,7 +118,7 @@ def zookeeper_rolling_restart():
                     print zookeeper_status
                     exit(1)
           
-    # Correct the server id in zoo.cfg for the new nodes in the zookeeper quorum
+    print "Correct the server id in zoo.cfg for the new nodes in the zookeeper quorum"
     with settings(host_string=database_nodes[0], password=env.passwords[database_nodes[0]]):
         run("sed -i '/^server.*3888/d' %s" % zoo_cfg)
         for zookeeper_node in database_nodes:
@@ -121,30 +127,38 @@ def zookeeper_rolling_restart():
         tmp_dir= tempfile.mkdtemp()
         get(zoo_cfg, tmp_dir)
 
-    # Correct the myid in myid file for the new nodes in the zookeeper quorum
+    print "Correct the myid in myid file for the new nodes in the zookeeper quorum"
     for zookeeper_node in database_nodes:
         zk_index = (database_nodes.index(zookeeper_node) + 1)
         with settings(host_string=zookeeper_node, password=env.passwords[zookeeper_node]):
-            #put cluster-unique zookeeper's instance id in myid
+            print "put cluster-unique zookeeper's instance id in myid"
             run('sudo echo "%s" > /var/lib/zookeeper/myid' % (zk_index))
-            run("service zookeeper stop")
+            run(stop_cmd)
 
-    # Restart all the zookeeper nodes in the new quorum
+    print "Restart all the zookeeper nodes in the new quorum"
     for zookeeper_node in database_nodes:
         with settings(host_string=zookeeper_node, password=env.passwords[zookeeper_node]):
-            restart_cmd = "/usr/lib/zookeeper/bin/zkServer.sh restart"
-            if detect_ostype() in ['Ubuntu']:
-                restart_cmd = "/usr/share/zookeeper/bin/zkServer.sh restart"
             put(tmp_dir+'/zoo.cfg', zoo_cfg)
             run(restart_cmd)
 
-    # Mkae sure leader/folower election is complete
+    print "Make sure leader/folower election is complete"
     with settings(host_string=zookeeper_node, password=env.passwords[zookeeper_node]):
         retries = 3
         while retries:
             zookeeper_status = verfiy_zookeeper(*database_nodes)
-            if 'leader' in zookeeper_status.values() and 'standalone' not in zookeeper_status.values():
+            if (len(database_nodes) > 1 and
+                'leader' in zookeeper_status.values() and
+                'follower' in zookeeper_status.values() and
+                'notrunning' not in zookeeper_status.values() and
+                'notinstalled' not in zookeeper_status.values() and
+                'standalone' not in zookeeper_status.values()):
                 print zookeeper_status
+                break
+            elif (len(database_nodes) == 1 and
+                'notinstalled' not in zookeeper_status.values() and
+                'standalone' in zookeeper_status.values()):
+                print zookeeper_status
+                print "Zookeeper quorum is already formed properly."
                 break
             else:
                 retries -= 1
