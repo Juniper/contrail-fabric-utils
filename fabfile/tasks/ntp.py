2d2f9aa67afbe83dcdc853c7b3543d32c85a8b04
Ignatious Johnson <ijohnson@juniper.net>
2014-09-24 17:09:57 -0700

    Pre check to make sure the time is synced bwteen the nodes 	Abort install_contrail and setup_all if time is not synced between nodes.
    Change-Id: Ic411b5df4d067866be42d8987b69067e2502f53e

---
+++ fabfile/tasks/ntp.py
@@ -0,0 +1,22 @@
+from fabfile.config import *
+
+@task
+@roles('all')
+def get_all_time():
+    date = run("DATE=$( sudo date ); DATEMILLISEC=$( sudo date +%s ); echo $DATE; echo $DATEMILLISEC")
+    return tuple(date.split('\r\n'))
+
+@task
+@roles('build')
+def verify_time_all():
+    result = execute('get_all_time')
+    print result
+    all_time = [int(date_in_millisec) for date, date_in_millisec in result.values()]
+    all_time.sort()
+   
+    if (all_time[-1] - all_time[0]) > 120:
+        raise RuntimeError("Time not synced in the nodes, Please sync and proceed:\n %s" % result)
+    else:
+        print "Time synced in the nodes, Proceeding to install/provision."
+
+     
