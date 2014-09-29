d446fdaa1e038e3ddf94bc685c5b36aa9d916cf8
Nagendra Chandran <npchandran@juniper.net>
2014-09-29 01:16:38 -0700

    Adding redhat_three_node_testbed.py
    Change-Id: I567f35fdbb2924e20753bf551399db14d1e1a187

---
+++ fabfile/testbeds/redhat_three_node_testbed.py
@@ -0,0 +1,186 @@
+from fabric.api import env
+
+#Management ip addresses of hosts in the cluster
+host1 = 'root@10.84.12.14'
+host2 = 'root@10.84.12.15'
+host3 = 'root@10.84.12.11'
+
+#External routers if any
+#for eg. 
+#ext_routers = [('mx1', '10.204.216.253')]
+ext_routers = []
+
+#Autonomous system number
+router_asn = 64512
+
+#Host from which the fab commands are triggered to install and provision
+host_build = 'stack@10.84.24.64'
+
+#Role definition of the hosts.
+env.roledefs = {
+    'all': [host1, host2, host3],
+    'cfgm': [host1],
+    'openstack': [host2],
+    'control': [host1],
+    'compute': [host3],
+    'collector': [host1],
+    'webui': [host1],
+    'database': [host1],
+    'build': [host_build],
+    'storage-master': [host1],
+    'storage-compute': [host1],
+}
+
+#Openstack admin password
+env.openstack_admin_password = 'c0ntrail123'
+
+#Hostnames
+env.hostnames = {
+    'all': ['a3s40', 'a3s39', 'a3s43']
+}
+
+env.password = 'c0ntrail123'
+#Passwords of each host
+env.passwords = {
+    host1: 'c0ntrail123',
+    host2: 'c0ntrail123',
+    host3: 'c0ntrail123',
+    host_build: 'contrail123'
+}
+
+#For reimage purpose
+env.ostypes = {
+    host1: 'redhat',
+    host2: 'redhat',
+    host3: 'redhat',
+}
+
+#OPTIONAL ANALYTICS CONFIGURATION
+#================================
+# database_dir is the directory where cassandra data is stored
+#
+# If it is not passed, we will use cassandra's default
+# /var/lib/cassandra/data
+#
+#database_dir = '<separate-partition>/cassandra'
+#
+# analytics_data_dir is the directory where cassandra data for analytics
+# is stored. This is used to seperate cassandra's main data storage [internal
+# use and config data] with analytics data. That way critical cassandra's 
+# system data and config data are not overrun by analytis data
+#
+# If it is not passed, we will use cassandra's default
+# /var/lib/cassandra/data
+#
+#analytics_data_dir = '<separate-partition>/analytics_data'
+#
+# ssd_data_dir is the directory where cassandra can store fast retrievable
+# temporary files (commit_logs). Giving cassandra an ssd disk for this
+# purpose improves cassandra performance
+#
+# If it is not passed, we will use cassandra's default
+# /var/lib/cassandra/commit_logs
+#
+#ssd_data_dir = '<seperate-partition>/commit_logs_data'
+
+#OPTIONAL BONDING CONFIGURATION
+#==============================
+#Inferface Bonding
+#bond= {
+#    host1 : { 'name': 'bond0', 'member': ['p2p0p0','p2p0p1','p2p0p2','p2p0p3'], 'mode': '802.3ad', 'xmit_hash_policy': 'layer3+4' },
+#}
+
+#OPTIONAL SEPARATION OF MANAGEMENT AND CONTROL + DATA and OPTIONAL VLAN INFORMATION
+#==================================================================================
+#control_data = {
+#    host1 : { 'ip': '192.168.10.1/24', 'gw' : '192.168.10.254', 'device': 'bond0', 'vlan': '224' },
+#}
+
+#OPTIONAL STATIC ROUTE CONFIGURATION
+#===================================
+#static_route  = {
+#    host1 : [{ 'ip': '10.1.1.0', 'netmask' : '255.255.255.0', 'gw':'192.168.10.254', 'intf': 'bond0' },
+#             { 'ip': '10.1.2.0', 'netmask' : '255.255.255.0', 'gw':'192.168.10.254', 'intf': 'bond0' }],
+#}
+
+#storage compute disk config
+#storage_node_config = {
+#    host1 : { 'disks' : ['sdc', 'sdd'] },
+#}
+
+#live migration config
+#live_migration = True
+
+
+#To disable installing contrail interface rename package
+env.interface_rename = False
+
+#In environments where keystone is deployed outside of Contrail provisioning
+#scripts , you can use the below options 
+#
+# Note : 
+# "insecure" is applicable only when protocol is https
+# The entries in env.keystone overrides the below options which used 
+# to be supported earlier :
+#  service_token
+#  keystone_ip
+#  keystone_admin_user
+#  keystone_admin_password
+#  region_name
+#
+env.keystone = {
+    'keystone_ip'   : '10.84.12.15',
+    'auth_protocol' : 'http',                  #Default is http
+    'auth_port'     : '35357',                 #Default is 35357
+    'admin_token' : '1232323223',
+    'admin_user'    : 'admin',                 #Default is admin
+    'admin_password': 'c0ntrail123',           #Default is contrail123
+    'service_tenant': 'service',               #Default is service
+    'admin_tenant'  : 'admin',                 #Default is admin
+    'region_name'   : 'RegionOne',             #Default is RegionOne
+    'insecure'      : 'True',                  #Default = False
+}
+#
+
+# In environments where openstack services are deployed independently 
+# from contrail, you can use the below options 
+# service_token : Common service token for for all services like nova,
+#                 neutron, glance, cinder etc
+# amqp_host     : IP of AMQP Server to be used in openstack
+#
+
+# old
+# 'service_token' : '15ee68dbae3b4416a7fda3400e0a6683', 
+env.openstack = {
+    'service_token' : 'a55e1eb7680d4d4eb092698480ab31f7',
+    'amqp_host' : '10.84.12.15',
+}
+
+# Neutron specific configuration 
+#env.neutron = {
+#   'protocol': 'http', # Default is http
+#}
+
+#To enable multi-tenancy feature
+#multi_tenancy = True
+
+#To enable haproxy feature
+#haproxy = True
+
+#To Enable prallel execution of task in multiple nodes
+#do_parallel = True
+
+# To configure the encapsulation priority. Default: MPLSoGRE 
+#env.encap_priority =  "'MPLSoUDP','MPLSoGRE','VXLAN'"
+
+# Optional proxy settings.
+# env.http_proxy = os.environ.get('http_proxy')
+
+env.test_repo_dir="/home/stack/redhat_sanity/multi_node/contrail-test"
+env.mail_from='npchandran@juniper.net'
+env.mail_to='dl-contrail-sw@juniper.net'
+multi_tenancy=True
+env.encap_priority="'MPLSoUDP','MPLSoGRE','VXLAN'"
+env.mail_server='10.84.24.64'
+env.mail_port='4000'
+env.log_scenario='Redhat70_Two_Node_Sanity_[CONTRAIL_ALL_ROLES_PLUS_RDO]'
