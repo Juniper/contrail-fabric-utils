023368b8bbc0f179864096b62ab57b69e1543317
Ignatious Johnson <ijohnson@juniper.net>
2014-10-01 15:26:08 -0700

    Fix bug 1373264 	Changing supervisor group name port for rabbitmq-server service.
    Change-Id: I505f4a394f0904b100f62ac8a95869804e3d25a9

--- fabfile/tasks/services.py
+++ fabfile/tasks/services.py
@@ -43,6 +43,7 @@ def start_cfgm():
     """starts the contrail config services."""
     with settings(warn_only=True):
         run('service supervisor-config start')
+        run('service supervisor-support-service start')
 
 @task
 @roles('database')
@@ -167,6 +168,7 @@ def restart_cfgm_node(*args):
     for host_string in args:
         with  settings(host_string=host_string):
             run('service supervisor-config restart')
+            run('service supervisor-support-service restart')
 
 @task
 @roles('control')
