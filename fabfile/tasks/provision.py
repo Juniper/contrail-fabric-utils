b850ba9f74fb8d2f3a643da929dc075d683a38c4
Nagendra Chandran <npchandran@juniper.net>
2014-10-17 12:28:23 -0700

    Reset fab connections to openstack so that new connections per session be created when running in parallel mode
    In Parallel mode, fab scripts are hung when reusing existing connection by multiple session.
    To avoid this, remove the connections to openstack from the connections dict. Now get commands will
    create new connections and they independently
    
    Change-Id: Idda9509e52a1e7f43c6c33e19a3bd627aded72ae

--- fabfile/tasks/provision.py
+++ fabfile/tasks/provision.py
@@ -1084,9 +1084,14 @@ def setup_only_vrouter_node(manage_nova_compute='yes', *args):
     #    print "contrail-agent package not installed. Install it and then run setup_vrouter"
     #    return
     
-    
+    # reset openstack connections to create new connections
+    # when running in parallel mode
+    openstack_host = env.roledefs['openstack'][0]
+    openstack_host_connection = openstack_host + ':22'
+    if connections and openstack_host_connection in connections.keys():
+        connections.pop(openstack_host_connection)
     # retrieve neutron_metadata_proxy_shared_secret from openstack
-    with settings(host_string=env.roledefs['openstack'][0]):
+    with settings(host_string=openstack_host):
         metadata_secret = get_value(src_file='/etc/nova/nova.conf',
                           section='keystone_authtoken',
                           variable='neutron_metadata_proxy_shared_secret')
