adc405dfe49da1f0a79ddee69c6f67bd598da653
Kiran Desai <kirand@juniper.net>
2014-10-22 14:19:56 -0700

    P3:Changes to provisioning modules to enable vcenter and esxi(complete) provisioning. Adding 2 new tasks 'prov_vcenter' and 'prov_esxi_computevm', along with a  new example testbed file 'testbed_vcenter_esxi'. The earlier mode of 'esxi' only is untouched. Additional helper tasks have been added in vmware.py to gather testbed information and provision. esxi_prov.py has been borrowed from 'server-manager' scripts. No modifications have been done to that script, as in the current state it works as expected.
    Change-Id: I0c1f37190ce0f1607ef955a85a22729daac5cb79

--- fabfile/tasks/vmware.py
+++ fabfile/tasks/vmware.py
@@ -3,6 +3,9 @@ import re
 
 from fabfile.config import *
 from fabfile.templates import compute_ovf_template
+from fabfile.tasks.install import yum_install, apt_install
+from esxi_prov import ContrailVM as ContrailVM
+from vcenter_prov import Vcenter as Vcenter
 
 def _get_var(var, default=None):
     try:
@@ -63,3 +66,62 @@ def _template_substitute_write(template, vals, filename):
     outfile.write(data)
     outfile.close()
 #end _template_substitute_write
+
+@task
+def provision_vcenter(vcenter_info, esxi_info):
+        apt_install(contrail-vmware-utils)
+        vcenter_params = {}
+        vcenter_params['server'] = vcenter_info['server']
+        vcenter_params['username'] = vcenter_info['username']
+        vcenter_params['password'] = vcenter_info['password']
+
+        vcenter_params['datacenter_name'] = vcenter_info['datacenter']
+        vcenter_params['cluster_name'] = vcenter_info['cluster']
+        vcenter_params['dvswitch_name'] = vcenter_info['dv_switch']['dv_switch_name']
+        vcenter_params['dvswitch_nic'] = vcenter_info['dv_switch']['nic']
+        vcenter_params['dvportgroup_name'] = vcenter_info['dv_port_group']['dv_portgroup_name']
+        vcenter_params['dvportgroup_num_ports'] = vcenter_info['dv_port_group']['number_of_ports']
+        hosts = []
+        for host in esxi_info.keys():
+                esxi_data = esxi_info[host]
+                data = esxi_data['esxi']
+
+                esx_list=[data['esx_ip'],data['esx_username'],data['esx_password'],data['esx_ssl_thumbprint']]
+                hosts.append(esx_list)
+
+        vcenter_params['hosts'] = hosts
+
+        Vcenter(vcenter_params)
+
+
+@task
+def provision_esxi(compute_vm_info):
+            vm_params = {}
+            vm_params['vm'] = compute_vm_info['esx_vm_name']
+            vm_params['vmdk'] = _get_var(compute_vm_info['vmdk'])
+            vm_params['datastore'] = compute_vm_info['esx_datastore']
+            vm_params['eth0_mac'] = _get_var(compute_vm_info['server_mac'])
+            vm_params['eth0_ip'] = _get_var(compute_vm_info['server_ip'])
+            vm_params['eth0_pg'] = _get_var(compute_vm_info['esxi']['esx_fab_port_group'])
+            vm_params['eth0_vswitch'] = _get_var(compute_vm_info['esxi']['esx_fab_vswitch'])
+            vm_params['eth0_vlan'] = None
+            vm_params['eth1_vswitch'] = _get_var(compute_vm_info['esx_vm_vswitch'])
+            vm_params['eth1_pg'] = _get_var(compute_vm_info['esx_vm_port_group'])
+            vm_params['eth1_vlan'] = "4095"
+            vm_params['uplink_nic'] = _get_var(compute_vm_info['esxi']['esx_uplink_nic'])
+            vm_params['uplink_vswitch'] = _get_var(compute_vm_info['esxi']['esx_fab_vswitch'])
+            vm_params['server'] = _get_var(compute_vm_info['esxi']['esx_ip'])
+            vm_params['username'] = _get_var(compute_vm_info['esxi']['esx_username'])
+            vm_params['password'] = _get_var(compute_vm_info['esxi']['esx_password'])
+            vm_params['thindisk'] =  _get_var(compute_vm_info['esx_vmdk'])
+            vm_params['domain'] =  _get_var(compute_vm_info['domain'])
+            vm_params['vm_password'] = _get_var(compute_vm_info['password'])
+            vm_params['vm_server'] = _get_var(compute_vm_info['server_id'])
+            vm_params['vm_deb'] = _get_var(compute_vm_info['vm_deb'])
+            out = ContrailVM(vm_params)
+            print out
+
+
+
+
+                                  
