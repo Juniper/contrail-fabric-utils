8848bf9c7f84aaa49a7572f247865fde2ed00727
Vedu Joshi <vjoshi@juniper.net>
2014-10-23 01:23:13 +0530

    Adding a new single-node testbed with nodec27
    Change-Id: Ia7461a70f19a6e4718b9684da0c5adb41861a953

---
+++ fabfile/testbeds/testbed_nodec27.py
@@ -0,0 +1,236 @@
+from fabric.api import env
+
+#Management ip addresses of hosts in the cluster
+host1 = 'root@10.204.217.12'
+
+
+#External routers if any
+#for eg. 
+#ext_routers = [('mx1', '10.204.216.253')]
+ext_routers = [('mx1','10.204.216.253')]
+
+#Autonomous system number
+router_asn = 64510
+public_vn_rtgt = 10003
+public_vn_subnet = '10.204.219.160/29'
+
+#Host from which the fab commands are triggered to install and provision
+host_build = 'root@10.204.217.12'
+
+
+#Role definition of the hosts.
+env.roledefs = {
+    'all': [host1],
+    'cfgm': [host1],
+    'openstack': [host1],
+    'control': [host1],
+    'compute': [host1],
+    'collector': [host1],
+    'webui': [host1],
+    'database': [host1],
+    'build': [host_build],
+}
+
+#Openstack admin password
+env.openstack_admin_password = 'contrail123'
+
+#Hostnames
+env.hostnames = {
+    'all': ['nodec27']
+}
+
+env.password = 'c0ntrail123'
+#Passwords of each host
+env.passwords = {
+    host1: 'c0ntrail123',
+    host_build: 'c0ntrail123',
+}
+
+#For reimage purpose
+env.ostypes = {
+    host1:'centos',
+}
+
+
+# INFORMATION FOR DB BACKUP/RESTORE ..
+#=======================================================
+#Optional,Backup Host configuration if it is not available then it will put in localhost
+#backup_node = 'root@2.2.2.2'
+
+# Optional, Local/Remote location of backup_data path 
+# if it is not passed it will use default path 
+#backup_db_path= ['/home/','/root/']
+#cassandra backup can be defined either "full" or "custom"  
+#full -> take complete snapshot of cassandra DB 
+#custom -> take snapshot except defined in skip_keyspace 
+#cassandra_backup='custom'  [ MUST OPTION] 
+#skip_keyspace=["ContrailAnalytics"]  IF cassandra_backup is selected as custom
+#service token need to define to do  restore of  backup data
+#service_token = '53468cf7552bbdc3b94f'
+
+
+#OPTIONAL ANALYTICS CONFIGURATION
+#================================
+# database_dir is the directory where cassandra data is stored
+#
+# If it is not passed, we will use cassandra's default
+# /var/lib/cassandra/data
+#
+#database_dir = '<separate-partition>/cassandra'
+#
+# analytics_data_dir is the directory where cassandra data for analytics
+# is stored. This is used to seperate cassandra's main data storage [internal
+# use and config data] with analytics data. That way critical cassandra's 
+# system data and config data are not overrun by analytis data
+#
+# If it is not passed, we will use cassandra's default
+# /var/lib/cassandra/data
+#
+#analytics_data_dir = '<separate-partition>/analytics_data'
+#
+# ssd_data_dir is the directory where cassandra can store fast retrievable
+# temporary files (commit_logs). Giving cassandra an ssd disk for this
+# purpose improves cassandra performance
+#
+# If it is not passed, we will use cassandra's default
+# /var/lib/cassandra/commit_logs
+#
+#ssd_data_dir = '<seperate-partition>/commit_logs_data'
+
+#OPTIONAL BONDING CONFIGURATION
+#==============================
+#Inferface Bonding
+#bond= {
+#    host1 : { 'name': 'bond0', 'member': ['p2p0p0','p2p0p1','p2p0p2','p2p0p3'], 'mode': '802.3ad', 'xmit_hash_policy': 'layer3+4' },
+#}
+
+#OPTIONAL SEPARATION OF MANAGEMENT AND CONTROL + DATA and OPTIONAL VLAN INFORMATION
+#==================================================================================
+#control_data = {
+#    host1 : { 'ip': '192.168.10.1/24', 'gw' : '192.168.10.254', 'device': 'bond0', 'vlan': '224' },
+#}
+
+#OPTIONAL STATIC ROUTE CONFIGURATION
+#===================================
+#static_route  = {
+#    host1 : [{ 'ip': '10.1.1.0', 'netmask' : '255.255.255.0', 'gw':'192.168.10.254', 'intf': 'bond0' },
+#             { 'ip': '10.1.2.0', 'netmask' : '255.255.255.0', 'gw':'192.168.10.254', 'intf': 'bond0' }],
+#}
+
+#storage compute disk config
+#storage_node_config = {
+#    host1 : { 'disks' : ['sdc', 'sdd'] },
+#}
+
+#live migration config
+#live_migration = True
+
+
+#To disable installing contrail interface rename package
+#env.interface_rename = False
+
+#In environments where keystone is deployed outside of Contrail provisioning
+#scripts , you can use the below options 
+#
+# Note : 
+# "insecure" is applicable only when protocol is https
+# The entries in env.keystone overrides the below options which used 
+# to be supported earlier :
+#  service_token
+#  keystone_ip
+#  keystone_admin_user
+#  keystone_admin_password
+#  region_name
+#
+#env.keystone = {
+#    'keystone_ip'   : 'x.y.z.a',
+#    'auth_protocol' : 'http',                  #Default is http
+#    'auth_port'     : '35357',                 #Default is 35357
+#    'admin_token'   : '33c57636fbc2c5552fd2',  #admin_token in keystone.conf
+#    'admin_user'    : 'admin',                 #Default is admin
+#    'admin_password': 'contrail123',           #Default is contrail123
+#    'service_tenant': 'service',               #Default is service
+#    'admin_tenant'  : 'admin',                 #Default is admin
+#    'region_name'   : 'RegionOne',             #Default is RegionOne
+#    'insecure'      : 'True',                  #Default = False
+#    'manage_neutron': 'no',                    #Default = 'yes' , Does configure neutron user/role in keystone required.
+#}
+#
+
+# In High Availability setups.
+#env.ha = {
+#    'internal_vip'   : '10.204.217.12',               #Internal Virtual IP of the HA setup.
+#    'external_vip'   : '2.2.2.2',               #External Virtual IP of the HA setup.
+#    'nfs_server'      : '3.3.3.3',               #IP address of the NFS Server which will be mounted to /var/lib/glance/images of openstack Node, Defaults to env.roledefs['compute'][0]
+#    'nfs_glance_path' : '/var/tmp/images/',      #NFS Server path to save images, Defaults to /var/tmp/glance-images/
+#}
+
+# In environments where openstack services are deployed independently 
+# from contrail, you can use the below options 
+# service_token : Common service token for for all services like nova,
+#                 neutron, glance, cinder etc
+# amqp_host     : IP of AMQP Server to be used in openstack
+# manage_amqp   : Default = 'no', if set to 'yes' provision's amqp in openstack nodes and
+#                 openstack services uses the amqp in openstack nodes instead of config nodes.
+#                 amqp_host is neglected if manage_amqp is set
+#
+#env.openstack = {
+#    'service_token' : '33c57636fbc2c5552fd2', #Common service token for for all openstack services
+#    'amqp_host' : '10.204.217.19',            #IP of AMQP Server to be used in openstack
+#    'manage_amqp' : 'yes',                    #Default no, Manage seperate AMQP for openstack services in openstack nodes.
+#}
+
+# Neutron specific configuration 
+#env.neutron = {
+#   'protocol': 'http', # Default is http
+#}
+
+#To enable multi-tenancy feature
+#multi_tenancy = True
+
+#To enable haproxy feature
+#haproxy = True
+
+#To Enable prallel execution of task in multiple nodes
+#do_parallel = True
+
+# To configure the encapsulation priority. Default: MPLSoGRE 
+#env.encap_priority =  "'MPLSoUDP','MPLSoGRE','VXLAN'"
+
+# Optional proxy settings.
+# env.http_proxy = os.environ.get('http_proxy')
+
+
+#OPTIONAL REMOTE SYSLOG CONFIGURATION
+#===================================
+#For R1.10 this needs to be specified to enable rsyslog.
+#For Later releases this would be enabled as part of provisioning,
+#with following default values.
+#
+#port = 19876
+#protocol = tcp
+#collector = dynamic i.e. rsyslog clients will connect to servers in a round
+#                         robin fasion. For static collector all clients will
+#                         connect to a single collector. static - is a test
+#                         only option.
+#status = enable
+#
+#env.rsyslog_params = {'port':19876, 'proto':'tcp', 'collector':'dynamic', 'status':'enable'}
+
+#OPTIONAL Virtual gateway CONFIGURATION
+#=======================================
+
+#Section vgw is only relevant when you want to use virtual gateway feature. 
+#You can use one of your compute node as  gateway .
+
+#Definition for the Key used
+#-------------------------------------
+#vn: Virtual Network fully qualified name. This particular VN will be used by VGW.
+#ipam-subnets: Subnets used by vn. It can be single or multiple
+#gateway-routes: If any route is present then only those routes will be published
+#by VGW or Default route (0.0.0.0) will be published
+
+#env.vgw = {host1: {'vgw1':{'vn':'default-domain:admin:public:public', 'ipam-subnets': ['10.204.220.128/29', '10.204.220.136/29', 'gateway-routes': ['8.8.8.0/24', '1.1.1.0/24']}]},
+#                   'vgw2':{'vn':'default-domain:admin:public1:public1', 'ipam-subnets': ['10.204.220.144/29']}
+#          }
+
