73cefe7989d1c5e5583a821e194f0ad34dc573f4
spuru <spuru@juniper.net>
2014-10-26 22:50:16 -0700

    Added Storage HA support
    Closes-Bug: #1366216
    Feature: Storage HA support added over existing contrail HA nodes.
    Change:
    1. cinder backend rbd settngs done for openstack storage nodes.
    2. cinder template config set to cinder HA vip.
    3. global public_network set to newly added storage node address.
    4. new cursh map is loaded using crushtool.
    5. cinder rabbit host settings done for openstack storage nodes.
    6. skipped compute-restart on openstack storage nodes.
    Test: verified storage HA by adding 2 openstack storage nodes on
          storage-master role. HA Cinder volume properly attached to
          VMs.
    Change done based on Review comments:
    1. removed tabs in storage-ceph-storage.py
    2. grep -w added to match exact wip address
    3. "storage os" comment changed to "storage openstack"
    4. "cfg-hostlist" changed to "config-hosts" as per convention
    5. added comments for new parameters
    6. setup_storage call done for add_storage_node
    
    Change-Id: I32b2f0e3cccf8c5ea6c05be88ef71199befe6e9d

--- fabfile/tasks/storage/misc.py
+++ fabfile/tasks/storage/misc.py
@@ -3,6 +3,7 @@ import os
 from fabfile.config import *
 from fabfile.utils.fabos import *
 from fabfile.tasks.storage.install import install_storage_compute_node
+from fabfile.tasks.storage.install import create_storage_repo_node
 #from fabfile.tasks.storage.provision import setup_storage_node
 
 @task
@@ -10,6 +11,7 @@ def add_storage_node(*args):
     """Adds one/more new storage node to the existing cluster."""
     for host_string in args:
         with settings(host_string=host_string):
+            execute("create_storage_repo_node", env.host_string)
             execute("install_storage_compute_node", env.host_string)
             execute("setup_add_storage_compute_node", env.host_string)
 
