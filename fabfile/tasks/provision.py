caf56f610fd777be67f56333df6acfb0e5b4242f
Ignatious Johnson <ijohnson@juniper.net>
2014-12-02 17:39:36 -0800

    pty=False argument to the sudo call is not supported.
    Change-Id: I13558fd9a34e77a181ed2bbf320ab9100df0d36d
    Closes-Bug:1398631

--- fabfile/tasks/provision.py
+++ fabfile/tasks/provision.py
@@ -1031,7 +1031,7 @@ def setup_control_node(*args):
                 sudo("setup-vnc-control --self_ip %s --cfgm_ip %s --collector_ip %s" \
                      %(tgt_ip, cfgm_ip, collector_ip))
                 if detect_ostype() == 'centos':
-                    sudo("service contrail-control restart", pty=False)
+                    sudo("service contrail-control restart")
 #end setup_control
 
 @task
