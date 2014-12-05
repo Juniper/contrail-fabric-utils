404af6c15aac4e643317415ed36c8832aef7dfbf
Ignatious Johnson <ijohnson@juniper.net>
2014-12-04 23:04:20 -0800

    increase_ulimits task needs to be executed on the openstack nodes, so moving it to the setup_orchestrator task.
    Change-Id: I693c687531dea800f13bac0c2d970efcf9701e1b

--- fabfile/tasks/provision.py
+++ fabfile/tasks/provision.py
@@ -1458,6 +1458,7 @@ def cleanup_remote_syslog_node():
 def setup_orchestrator():
     orch = get_orchestrator()
     if orch == 'openstack':
+        execute('increase_ulimits')
         execute('setup_openstack')
         if get_openstack_internal_vip():
             execute('sync_keystone_ssl_certs')
@@ -1474,7 +1475,6 @@ def setup_all(reboot='True'):
     execute('setup_ha')
     execute('setup_rabbitmq_cluster')
     execute('increase_limits')
-    execute('increase_ulimits')
     execute('setup_database')
     execute('verify_database')
     execute('setup_orchestrator')
