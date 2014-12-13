59abb20c7b95c5eda562adc2cc3b33cd798e5072
Nagendra Chandran <npchandran@juniper.net>
2014-12-13 00:24:03 -0800

    Read from openstack node only if metadata_secret is not available in testbed
    Change-Id: I7adf6802e5cea239077581a6c49ddd2c6b6b7046
    Partial-Bug: 1402112

--- fabfile/utils/config.py
+++ fabfile/utils/config.py
@@ -1,25 +1,35 @@
+import os
 import shutil
 import tempfile
+import ConfigParser
 from ConfigParser import SafeConfigParser
 
 from fabfile.config import *
 
-def get_value(src_file, section, variable):
-    '''Retrieve value of given variable in the specified config file
-       from the first openstack node
+def get_value(src_file, section, *variables, **kwargs):
+    '''Retrieve value of given variables in the specified config file
+       from the node
     '''
+    values = []
+    raw = kwargs.get('raw', True)
     temp_dir = tempfile.mkdtemp()
-    dest_file = '%s/openstack_nova.conf' % temp_dir
+    dest_file = '%s/%s' % (temp_dir, os.path.basename(src_file))
     print 'INFO: Retrieve (%s) file from (%s) and copy to (%s)' %  (
            src_file,
            '%s' % env.host_string,
            dest_file)
     status = get(src_file, dest_file)
-    # Parse file and retrieve secret
+    # Parse file and retrieve values
     parser = SafeConfigParser()
     read_files = parser.read(dest_file)
     if dest_file not in read_files:
         raise RuntimeError('ERROR: Unable to parse (%s) ...' % dest_file)
-    section_vars = dict(parser.items(section))
+    for variable_name in variables:
+        try:
+            values.append(parser.get(section, variable_name, raw=raw))
+        except (ConfigParser.NoOptionError, ConfigParser.NoSectionError) as err:
+            print "WARNING: Exception (%s) during retrieving (%s)"\
+                  " from section (%s)" % (err, variable_name, section)
+            values.append(None)
     shutil.rmtree(temp_dir)
-    return section_vars.get(variable)
+    return values[0] if len(values) == 1 else values
