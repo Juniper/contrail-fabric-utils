7a60be9803844e985d6a2e10e06b7c040fc9a336
Nagendra Chandran <npchandran@juniper.net>
2014-12-18 00:11:20 -0800

    Adding Restore repo fab task
    This fab task can revert all backup repo files
    
    Change-Id: I9b7622160f93fc272102de877ec75258a115fd8a

--- fabfile/contraillabs/utils.py
+++ fabfile/contraillabs/utils.py
@@ -1,3 +1,5 @@
+import re
+
 from fabfile.config import *
 from fabfile.utils.fabos import detect_ostype
 
@@ -5,7 +7,9 @@ from fabfile.utils.fabos import detect_ostype
 @EXECUTE_TASK
 @roles('all')
 def install_test_repo():
-    '''Installs test repo which contains packages specific to execute contrail test scripts'''
+    '''Installs test repo which contains packages specific to
+       execute contrail test scripts
+    '''
     execute('install_test_repo_node', env.host_string)
 
 @task
@@ -41,12 +45,13 @@ def cleanup_repo_node(*args):
                 ts = sudo("date '+%s'")
             if os_type == 'ubuntu':
                 org_repo_file = '/etc/apt/sources.list'
-                bck_repo_file = '/etc/apt/sources_%s.list' % ts
+                bck_repo_file = '/etc/apt/sources.list.%s.contrail' % ts
                 print 'Backup Original sources.list (%s) as (%s)' % (
                     org_repo_file, bck_repo_file)
                 sudo('mv %s %s' % (org_repo_file, bck_repo_file))
                 print 'Create an empty sources.list'
                 sudo('touch /etc/apt/sources.list')
+                sudo('echo >> /etc/apt/sources.list')
                 sudo('chmod 644 /etc/apt/sources.list')
                 sudo('apt-get update')
             elif os_type in ['centos', 'redhat']:
@@ -56,8 +61,42 @@ def cleanup_repo_node(*args):
                             repo_files = run('ls -1 *.repo').split('\r\n')
                         print 'Backup Original repo files with timestamp'
                         for repo_file in repo_files:
-                            sudo('mv %s %s.%s' % (repo_file, repo_file, ts))
+                            sudo('mv %s %s.%s.contrail' % (repo_file,
+                                 repo_file, ts))
                         sudo('yum clean all')
+
+@task
+@EXECUTE_TASK
+@roles('all')
+def restore_repo():
+    '''Removes all existing repos in all the nodes specified in testbed'''
+    execute('restore_repo_node', env.host_string)
+
+@task
+def restore_repo_node(*args):
+    '''Remove existing repos in given node'''
+    pattern = re.compile(r'\.[\d]+\.contrail$')
+    for host_string in args:
+        with settings(host_string=host_string, warn_only=True):
+            os_type = detect_ostype().lower()
+            if os_type == 'ubuntu':
+                dirname = '/etc/apt'
+                update_cmd = 'apt-get update'
+            else:
+                dirname = '/etc/yum.repos.d/'
+                update_cmd = 'yum clean all'
+
+            with cd(dirname):
+                repo_files = sudo("find . -maxdepth 1 -regextype sed \
+                                          -regex '.*/*[0-9]*\.contrail'")
+                repo_files = repo_files.split('\r\n')
+                restore_map = [(repo_file, pattern.sub('', repo_file)) \
+                                  for repo_file in repo_files]
+                for original, restore_file in restore_map:
+                    sudo('cp %s %s.oldbckup' % (original, original))
+                    sudo('mv %s %s' % (original, restore_file))
+                    sudo('chmod 644 %s' % restore_file)
+                sudo(update_cmd)
 
 @task
 def update_keystone_admin_token():
