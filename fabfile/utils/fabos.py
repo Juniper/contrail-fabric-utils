419f1e1a5b975d96ed6e9035356b8115cf556b17
Shaju V K <shajuvk@juniper.net>
2015-03-02 13:04:42 -0800

    setup_all was failing while copying haproxy.cfg to a directory in /tmp. fixing tempdir creation issue in mainline
    Change-Id: Ibe690489293a53657fad1b5295fac28d4f6695d0

--- fabfile/utils/fabos.py
+++ fabfile/utils/fabos.py
@@ -85,7 +85,7 @@ def get_as_sudo(src_file, dst_file):
     basename = os.path.basename(src_file.rstrip('/'))
     if not basename or basename in ['*']:
         raise Exception('%s is not a valid path'%src_file)
-    tempdir = tempfile.mkdtemp()
+    tempdir = run('(tempdir=$(mktemp -d); echo $tempdir)')
     tmp_file = '%s/tmp%s' % (tempdir, basename)
     sudo('cp -rf %s %s'%(src_file, tmp_file))
     sudo('chmod -R 777 %s'%tmp_file)
