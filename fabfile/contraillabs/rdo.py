eabbe90221d036cbed59f871f5eea27d2a0aa1df
Nagendra Chandran <npchandran@juniper.net>
2015-03-12 12:44:24 -0700

    Disable neutron-server and compute on startup in rhosp node and enable reboot_on_kernel_update
    Also adding new fab task - upgrade_kernel_without_openstack
    
    Change-Id: I4eca7520a39a1ed319c8ba488b81abec5dd32295
    
    Adding new fab tasks and update existing setup_rhosp_node task
    
    1. Added update_rhosp_node task to upgrade packages in redhat node to latest present in the repos
    2. Removed install_rhosp5_repo from setup_rhosp_node because there is going to be multiple repos
    3. Replaced run commands with sudo commands
    
    Change-Id: Ib57eb9864014d596de237ebb9bcf391e9f05a6a1

--- fabfile/contraillabs/rdo.py
+++ fabfile/contraillabs/rdo.py
@@ -2,6 +2,7 @@
 
 from fabfile.config import *
 from fabfile.utils.host import get_keystone_ip
+from fabfile.utils.cluster import reboot_nodes
 
 @task
 @roles('openstack')
@@ -14,60 +15,84 @@ def fix_yum_repos():
 @task
 @roles('openstack')
 def install_rhosp5_repo():
-    """ copy local repo locations """
-    put('fabfile/contraillabs/repo/rhosp5_local.repo', '/etc/yum.repos.d/rhosp5_local.repo')
-    run('yum clean all')
+    """ copy local rhosp5 repo locations """
+    put('fabfile/contraillabs/repo/rhosp5_local.repo', '/etc/yum.repos.d/rhosp5_local.repo', use_sudo=True)
+    sudo('yum clean all')
+
+@task
+@roles('openstack')
+def install_rhosp6_repo():
+    """ copy local rhosp6 repo locations """
+    put('fabfile/contraillabs/repo/rhosp6_local.repo', '/etc/yum.repos.d/rhosp6_local.repo', use_sudo=True)
+    sudo('yum clean all')
+
+@task
+@roles('openstack')
+def update_rhosp_node(reboot='True'):
+    with settings(warn_only=True):
+        sudo("yum -y install yum-utils")
+        sudo("yum -y install kernel-headers")
+        sudo("yum update -y")
+        print "[%s]: Disable NeworkManager and reboot" % env.host_string
+        sudo("systemctl stop NetworkManager")
+        sudo("systemctl disable NetworkManager")
+    if reboot == 'True':
+        execute(reboot_nodes, env.host_string)
+        pass
+    else:
+        print '[%s]: WARNING: Skipping Reboot as reboot!=True. '\
+              'Reboot manually to avoid misconfiguration!' % env.host_string
 
 @task
 @roles('openstack')
 def setup_rhosp_node():
     """Set up RHOSP Node"""
-    execute(install_rhosp5_repo)
-    with settings(warn_only=True):
-        run('sudo yum install -y openstack-packstack')
-    run('packstack --allinone --mariadb-pw=juniper123 --use-epel=n --nagios-install=n')
+    sudo('sudo yum install -y openstack-packstack')
+    sudo('packstack --allinone --mariadb-pw=juniper123 --use-epel=n --nagios-install=n')
     openstack_password = getattr(env, 'openstack_admin_password', 'contrail123')
-    run('source keystonerc_admin && keystone user-password-update --pass %s admin' % openstack_password)
-    run("sed -i -e 's/export OS_PASSWORD=.*/export OS_PASSWORD=%s/' keystonerc_admin " % openstack_password)
+    sudo('source keystonerc_admin && keystone user-password-update --pass %s admin' % openstack_password)
+    sudo("sed -i -e 's/export OS_PASSWORD=.*/export OS_PASSWORD=%s/' keystonerc_admin " % openstack_password)
     with settings(warn_only=True):
-        run("service openstack-nova-compute status")
-    run("service openstack-nova-compute stop")
+        sudo("service openstack-nova-compute status")
+    sudo("service openstack-nova-compute stop")
+    sudo("chkconfig openstack-nova-compute off")
     with settings(warn_only=True):
-        run("service openstack-nova-compute status")
+        sudo("service openstack-nova-compute status")
     with settings(warn_only=True):
-        run("service neutron-server status")
-    run("service neutron-server stop")
+        sudo("service neutron-server status")
+    sudo("service neutron-server stop")
+    sudo("chkconfig neutron-server off")
     with settings(warn_only=True):
-        run("service neutron-server status")
+        sudo("service neutron-server status")
     with settings(warn_only=True):
-        openstackrc_file = run("ls /etc/contrail/openstackrc")
+        openstackrc_file = sudo("ls /etc/contrail/openstackrc")
         if openstackrc_file.return_code != 0:
-            run('mkdir -p /etc/contrail/')
-            run("ln -s /root/keystonerc_admin /etc/contrail/openstackrc")
+            sudo('mkdir -p /etc/contrail/')
+            sudo("ln -s /root/keystonerc_admin /etc/contrail/openstackrc")
     cfgm_0_ip = testbed.env['roledefs']['cfgm'][0].split('@')[1]
     keystone_ip = get_keystone_ip()
-    run("source /etc/contrail/openstackrc; nova service-disable $(hostname) nova-compute")
-    run("openstack-config --set /etc/nova/nova.conf DEFAULT network_api_class nova.network.neutronv2.api.API")
-    run("openstack-config --set /etc/nova/nova.conf DEFAULT neutron_url http://%s:9696" % cfgm_0_ip)
-    run("openstack-config --set /etc/nova/nova.conf DEFAULT neutron_admin_auth_url http://%s:35357/v2.0" % keystone_ip)
-    run("openstack-config --set /etc/nova/nova.conf DEFAULT  compute_driver nova.virt.libvirt.LibvirtDriver")
-    run("openstack-config --set /etc/nova/nova.conf DEFAULT  novncproxy_port 5999")
-    run("service openstack-nova-api restart")
-    run("service openstack-nova-conductor restart")
-    run("service openstack-nova-scheduler restart")
-    run("service openstack-nova-novncproxy restart")
-    run("service openstack-nova-consoleauth restart")
-    run("iptables --flush")
-    run("sudo service iptables stop; echo pass")
-    run("sudo service ip6tables stop; echo pass")
-    run("sudo systemctl stop firewalld; echo pass")
-    run("sudo systemctl status firewalld; echo pass")
-    run("sudo chkconfig firewalld off; echo pass")
-    run("sudo /usr/libexec/iptables/iptables.init stop; echo pass")
-    run("sudo /usr/libexec/iptables/ip6tables.init stop; echo pass")
-    run("sudo service iptables save; echo pass")
-    run("sudo service ip6tables save; echo pass")
-    run("mkdir -p /var/crashes")
+    sudo("source /etc/contrail/openstackrc; nova service-disable $(hostname) nova-compute")
+    sudo("openstack-config --set /etc/nova/nova.conf DEFAULT network_api_class nova.network.neutronv2.api.API")
+    sudo("openstack-config --set /etc/nova/nova.conf DEFAULT neutron_url http://%s:9696" % cfgm_0_ip)
+    sudo("openstack-config --set /etc/nova/nova.conf DEFAULT neutron_admin_auth_url http://%s:35357/v2.0" % keystone_ip)
+    sudo("openstack-config --set /etc/nova/nova.conf DEFAULT  compute_driver nova.virt.libvirt.LibvirtDriver")
+    sudo("openstack-config --set /etc/nova/nova.conf DEFAULT  novncproxy_port 5999")
+    sudo("service openstack-nova-api restart")
+    sudo("service openstack-nova-conductor restart")
+    sudo("service openstack-nova-scheduler restart")
+    sudo("service openstack-nova-novncproxy restart")
+    sudo("service openstack-nova-consoleauth restart")
+    sudo("iptables --flush")
+    sudo("sudo service iptables stop; echo pass")
+    sudo("sudo service ip6tables stop; echo pass")
+    sudo("sudo systemctl stop firewalld; echo pass")
+    sudo("sudo systemctl status firewalld; echo pass")
+    sudo("sudo chkconfig firewalld off; echo pass")
+    sudo("sudo /usr/libexec/iptables/iptables.init stop; echo pass")
+    sudo("sudo /usr/libexec/iptables/ip6tables.init stop; echo pass")
+    sudo("sudo service iptables save; echo pass")
+    sudo("sudo service ip6tables save; echo pass")
+    sudo("mkdir -p /var/crashes")
 
     steps = "\n\n\n"
     steps += "="*160
