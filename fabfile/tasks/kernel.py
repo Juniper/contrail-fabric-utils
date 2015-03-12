6b45f7863cc9bfe7716b504727a7e586ce95c77f
Nagendra Chandran <npchandran@juniper.net>
2015-03-12 12:44:24 -0700

    Disable neutron-server and compute on startup in rhosp node and enable reboot_on_kernel_update
    Also adding new fab task - upgrade_kernel_without_openstack
    
    Change-Id: I4eca7520a39a1ed319c8ba488b81abec5dd32295

--- fabfile/tasks/kernel.py
+++ fabfile/tasks/kernel.py
@@ -1,7 +1,7 @@
 from fabfile.config import *
 from fabfile.utils.fabos import detect_ostype, get_linux_distro
-from fabfile.utils.cluster import get_nodes_to_upgrade_pkg, reboot_nodes
-from fabfile.tasks.install import apt_install
+from fabfile.utils.cluster import get_nodes_to_upgrade_pkg, reboot_nodes, get_package_installed_info
+from fabfile.tasks.install import apt_install, pkg_install
 
 
 @task
@@ -33,6 +33,39 @@ def upgrade_kernel_all(reboot='yes'):
             reboot_nodes(env.host_string)
         else:
             reboot_nodes(*nodes)
+
+@task
+@roles('build')
+def upgrade_kernel_without_openstack(reboot='yes'):
+    """creates repo and upgrades kernel"""
+    non_openstack_nodes = [node for node in env.roledefs['all'] if node not in env.roledefs['openstack']]
+    execute('create_install_repo_without_openstack')
+    nodes = []
+    with settings(host_string=env.roledefs['cfgm'][0], warn_only=True):
+        dist, version, extra = get_linux_distro()
+
+    if 'red hat' in dist.lower() and version == '7.0':
+        (package, os_type) = ('kernel-3.10.0-228.el7.x86_64', 'redhat')
+    else:
+        raise RuntimeError("Unsupported platfrom (%s, %s, %s) for"
+                           " kernel upgrade." % (dist, version, extra))
+    nodes = get_package_installed_info(package, os_type, *non_openstack_nodes)
+    if not nodes['not_installed']:
+        print "Nodes are already booted with expected version"
+        return
+    if nodes['installed']:
+        print "Nodes (%s) are already booted in expected "\
+              "kernel version" % ", ".join(nodes['installed'])
+
+    execute(upgrade_kernel_node, *nodes['not_installed'])
+    if reboot == 'yes':
+        if env.host_string in nodes:
+            nodes.remove(env.host_string).append(env.host_string)
+        reboot_nodes(*nodes['not_installed'])
+    else:
+        print "WARNING: Reboot Skipped as reboot=False; "\
+              "Reboot manually to avoid misconfiguration"
+
 @task
 @EXECUTE_TASK
 @roles('all')
@@ -46,8 +79,8 @@ def upgrade_kernel_node(*args):
     for host_string in args:
         with settings(host_string=host_string):
             dist, version, extra = get_linux_distro()
-            print "upgrading apparmor before upgrading kernel"
             if version == '12.04':
+                print "upgrading apparmor before upgrading kernel"
                 apt_install(["apparmor"])
                 print "Installing 3.13.0-34 kernel headers"
                 apt_install(["linux-headers-3.13.0-34"])
@@ -61,6 +94,12 @@ def upgrade_kernel_node(*args):
                 print "Upgrading the kernel to 3.13.0-40"
                 apt_install(["linux-image-3.13.0-40-generic",
                              "linux-image-extra-3.13.0-40-generic"])
+            elif 'red hat' in dist.lower() and version == '7.0':
+                print "Upgrading kernel to version 3.10.0-229"
+                pkg_install(["kernel-3.10.0-229.el7.x86_64",
+                             "kernel-tools-3.10.0-229.el7.x86_64",
+                             "kernel-tools-libs-3.10.0-229.el7.x86_64",
+                             "kernel-headers-3.10.0-229.el7.x86_64"], disablerepo=False)
 
 @task
 @EXECUTE_TASK
