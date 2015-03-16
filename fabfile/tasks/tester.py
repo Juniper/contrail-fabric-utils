4c4c0ac6dc33f351641618cd541e680c8f897229
Vedu Joshi <vjoshi@juniper.net>
2015-03-16 11:24:34 +0530

    Try to avoid TypeError while installing latest unittest2
    If we install linecache2 separately before installing latest unittest2,
    things go fine.
    
    Change-Id: I4e6b14357e74ec12158a71065b4ce9d31ebe46c7

--- fabfile/tasks/tester.py
+++ fabfile/tasks/tester.py
@@ -308,7 +308,11 @@ def setup_test_env():
                         pip install --upgrade unittest2 && \
                         pip install --upgrade %s' %pkg)
                 else:
-                    sudo("pip install --upgrade unittest2==0.8.0")
+                    # Avoid installing linecache2 as dependency on unittest2
+                    # Avoid "TypeError: dist must be a Distribution instance"
+                    sudo("pip install linecache2")
+
+                    sudo("pip install --upgrade unittest2")
                     sudo("pip install --upgrade %s" %pkg)
                 if not exists('/usr/bin/ant'):
                     pkg_install(['ant'],disablerepo = False)
