16b03eb2fcb2b088549f9d76ca201758b5364dcd
Sunil Basker <sunilbasker@juniper.net>
2015-03-23 13:46:27 +0530

    Restart NTP service incase the clocks aren't synced, before giving up, in verify_time_all
    Change-Id: I037eeb89d7386973c626753069c8a22b300ea984
    Closes-Bug: #1393295

--- fabfile/tasks/ntp.py
+++ fabfile/tasks/ntp.py
@@ -7,22 +7,33 @@ def get_all_time():
     return tuple(date.split('\r\n'))
 
 @task
+@roles('all')
+def restart_ntp():
+    sudo("service ntp restart")
+
+@task
 @parallel
 @roles('build')
 def verify_time_all():
-    result = execute('get_all_time')
-    all_time = []
-    for dates in result.values():
-        try:
-            (date, date_in_millisec) = dates
-            all_time.append(int(date_in_millisec))
-        except ValueError:
-            print "ERROR: %s" %  dates
-    all_time.sort()
+    for retry in (True, False):
+        result = execute('get_all_time')
+        all_time = []
+        for dates in result.values():
+            try:
+                (date, date_in_millisec) = dates
+                all_time.append(int(date_in_millisec))
+            except ValueError:
+                print "ERROR: %s" %  dates
+        all_time.sort()
 
-    if (all_time[-1] - all_time[0]) > 240:
+        if (all_time[-1] - all_time[0]) < 240:
+            break;
+        if retry:
+            execute('restart_ntp')
+            sleep(60)
+    else:
         raise RuntimeError("Time not synced in the nodes,"
                            " Please sync and proceed:\n %s %s %s" %
                            (result, all_time[-1], all_time[0]))
-    else:
-        print "Time synced in the nodes, Proceeding to install/provision."
+
+    print "Time synced in the nodes, Proceeding to install/provision."
