11d6d03a4239974679e25cb6dc410ee587fd9c77
Megh Bhatt <meghb@juniper.net>
2015-05-18 15:23:20 -0700

    1. Install and provision mongodb on database nodes instead of    openstack node. Provision mongodb replicaSet when using    multiple database nodes. Change ceilometer conf to use the    replicaSet. mongodb will be installed as part of    install_database. 2 new tasks - fixup_mongodb_conf and    setup_mongodb_ceilometer_cluster have been added for    mongodb provisioning. 2. Support ceilometer installation and provisioning only on    Juno onwards for contrail cloud. Closes-Bug: #1456373
    (cherry picked from commit 2a430fe7a38a5aecb4e5490b9017fe3dc7f2cb8f)
    
    Conflicts:
    
    	fabfile/utils/install.py
    
    Change-Id: I223883ddb02162faf0e719b88da70b4c725372f8

--- fabfile/utils/install.py
+++ fabfile/utils/install.py
@@ -89,28 +89,20 @@ def get_openstack_ceilometer_pkgs():
     """ Returns the list of ceilometer packages used in a
         openstack node.
     """
-    pkg_havana_ubuntu = ['mongodb', 'ceilometer-api',
+    pkg_juno_ubuntu = ['ceilometer-api',
         'ceilometer-collector',
         'ceilometer-agent-central',
-        'python-ceilometerclient']
-    pkg_icehouse_ubuntu = pkg_havana_ubuntu + [
         'ceilometer-agent-notification',
         'ceilometer-alarm-evaluator',
         'ceilometer-alarm-notifier',
-        'ceilometer-plugin-contrail']
-    pkg_juno_ubuntu = pkg_icehouse_ubuntu + [
-        'mongodb-server',
+        'ceilometer-plugin-contrail',
         'mongodb-clients',
         'python-pymongo']
-    pkg_juno_ubuntu.remove('mongodb')
-    pkg_icehouse_redhat = ['ceilometer-plugin-contrail']
+    pkg_redhat = ['ceilometer-plugin-contrail']
 
     ceilometer_pkgs = {
-        'ubuntu' : {'havana' : pkg_havana_ubuntu,
-                    'icehouse' : pkg_icehouse_ubuntu,
-                    'juno' : pkg_juno_ubuntu
-                   },
-        'redhat' : {'icehouse' : pkg_icehouse_redhat},
+        'ubuntu' : {'juno' : pkg_juno_ubuntu},
+        'redhat' : {'juno' : pkg_redhat},
     }
 
     act_os_type = detect_ostype()
@@ -123,10 +115,8 @@ def get_ceilometer_plugin_pkgs():
     """
     pkg_contrail_ceilometer = ['ceilometer-plugin-contrail']
     ceilometer_plugin_pkgs = {
-        'ubuntu' : {'icehouse' : pkg_contrail_ceilometer,
-                    'juno' : pkg_contrail_ceilometer
-                   },
-        'redhat' : {'icehouse' : pkg_contrail_ceilometer},
+        'ubuntu' : {'juno' : pkg_contrail_ceilometer},
+        'redhat' : {'juno' : pkg_contrail_ceilometer},
     }
 
     act_os_type = detect_ostype()
