cfa7fd324f0cec559d100cce6a5ba901ba588876
Ignatious Johnson Christopher <ijohnson@juniper.net>
2015-11-05 15:05:34 -0800

    Comparing the release version using distutils.version.LooseVersion during upgrade, this is required to support upgrade to maintanence releases. Partial-Bug: 1513633
    Change-Id: I48b2cef574ea77a61b1fb0a90faf8bc7dc308994
    (cherry picked from commit 6d63f013274bafaa2a9f5d713e6b923db9c20cdf)

--- fabfile/tasks/upgrade.py
+++ fabfile/tasks/upgrade.py
@@ -1,4 +1,5 @@
 from fabfile.config import *
+from distutils.version import LooseVersion
 
 from fabfile.tasks.install import pkg_install, install_contrail_vcenter_plugin
 from fabfile.tasks.provision import fixup_restart_haproxy_in_all_cfgm
@@ -72,7 +73,8 @@ def upgrade_config_node(from_rel, pkg, *args):
                 install_contrail_vcenter_plugin(pkg)
 
             #Downgrading keepalived as we are packaging lower version of keepalivd in R2.20
-            if (float(from_rel) == 2.2 and get_release() >= 2.2):
+            if (LooseVersion(from_rel) == LooseVersion('2.20') and
+                LooseVersion(get_release()) >= LooseVersion('2.20')):
                 dist, version, extra = get_linux_distro()
                 if version == '14.04':
                     cmd = 'DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes'
@@ -177,7 +179,7 @@ def upgrade_compute_node(from_rel, pkg, *args):
             if (getattr(env, 'interface_rename', True) and
                 detect_ostype() not in ['ubuntu', 'redhat']):
                pkgs.append('contrail-interface-name')
-            if float(from_rel) < 2.1:
+            if LooseVersion(from_rel) < LooseVersion('2.10'):
                 dist, version, extra = get_linux_distro()
                 if version == '14.04' and 'contrail-vrouter-3.13.0-35-generic' in pkgs:
                     pkgs.remove('contrail-vrouter-3.13.0-35-generic')
@@ -246,7 +248,7 @@ def upgrade_contrail(from_rel, pkg, orch='yes'):
     execute('upgrade_webui', from_rel, pkg)
     execute('upgrade_compute', from_rel, pkg)
     # Adding config, database and analytics nodes to api-server
-    if float(from_rel) < 2.2:
+    if LooseVersion(from_rel) < LooseVersion('2.20'):
         execute('prov_config')
         execute('prov_database')
         execute('prov_analytics')
