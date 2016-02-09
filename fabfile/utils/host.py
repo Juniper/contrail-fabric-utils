c679b099dab5ee987b3af1fa6a2119f6b8f50c8c
Ignatious Johnson Christopher <ijohnson@juniper.net>
2016-02-09 13:29:59 -0800

    SSL support for openstack and api-server
    Change-Id: Ib86c1a2ff70191d4681641e61634637f7f6e869d
    Partial-bug: 1547965

--- fabfile/utils/host.py
+++ fabfile/utils/host.py
@@ -197,13 +197,16 @@ def get_authserver_protocol():
        for k in env.vcenter_servers.keys():
            return get_from_vcenter_dict(k, 'auth', 'https')
     # openstack
-    return get_from_testbed_dict('keystone', 'auth_protocol','http')
+    auth_protocol = 'http'
+    if keystone_ssl_enabled():
+        auth_protocol = 'https'
+    return get_from_testbed_dict('keystone', 'auth_protocol', auth_protocol)
 
 def get_keystone_version():
     return get_from_testbed_dict('keystone', 'version', 'v2.0')
 
 def get_keystone_insecure_flag():
-    return get_from_testbed_dict('keystone', 'insecure', 'False')
+    return get_from_testbed_dict('keystone', 'insecure', 'True')
 
 def get_authserver_port():
     orch = getattr(env, 'orchestrator', 'openstack')
@@ -389,3 +392,41 @@ def get_bgp_md5(host = env.host_string):
     """
     return get_from_testbed_dict('md5', host, None)
 #end get_bgp_md5
+
+def get_keystone_certfile():
+    default = '/etc/keystone/ssl/certs/keystone.pem'
+    return get_from_testbed_dict('keystone','certfile', default)
+
+
+def get_keystone_keyfile():
+    default = '/etc/keystone/ssl/private/keystone.key'
+    return get_from_testbed_dict('keystone','keyfile', default)
+
+
+def get_keystone_cafile():
+    default = '/etc/keystone/ssl/certs/keystone_ca.pem'
+    return get_from_testbed_dict('keystone','cafile', default)
+
+
+def get_apiserver_certfile():
+    default = '/etc/contrail/ssl/certs/contrail.pem'
+    return get_from_testbed_dict('cfgm','certfile', default)
+
+
+def get_apiserver_keyfile():
+    default = '/etc/contrail/ssl/private/contrail.key'
+    return get_from_testbed_dict('cfgm','keyfile', default)
+
+
+def get_apiserver_cafile():
+    default = '/etc/contrail/ssl/certs/contrail_ca.pem'
+    return get_from_testbed_dict('cfgm','cafile', default)
+
+def keystone_ssl_enabled():
+    return get_from_testbed_dict('keystone', 'ssl', False)
+
+def apiserver_ssl_enabled():
+    return get_from_testbed_dict('cfgm', 'ssl', False)
+
+def get_apiserver_insecure_flag():
+    return get_from_testbed_dict('cfgm', 'insecure', 'True')
