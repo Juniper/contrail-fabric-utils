138e462edafe5277dd1def1f6c0645d589a95863
Aswani Kumar Reddy G <aswanikr@juniper.net>
2016-03-11 12:07:24 +0530

    skip snapshots of skipped_keyspaces if already present
    Change-Id: I4db01c61e4f08645e5e556a2cd158ef805bde67e
    Closes-Bug: #1555941

--- fabfile/tasks/backup_restore.py
+++ fabfile/tasks/backup_restore.py
@@ -166,6 +166,7 @@ def backup_cassandra(db_datas, store_db='local', cassandra_backup='full'):
     """Backup cassandra data in all databases  """
     global backup_path, final_dir
     snapshot_list=[]
+    skip_key=None
     host = env.host_string
     msg = "Processing the Cassandra DB backup and default path for backup DB data is ~/contrail_bkup_data/hostname/data/ in ({HOST})  \n"
     with settings(host_string=host):
@@ -201,10 +202,16 @@ def backup_cassandra(db_datas, store_db='local', cassandra_backup='full'):
                 cs_key = cs_key.translate(string.maketrans("\n\t\r", "   "))
                 custom_key = replace_key(cs_key, skip_key)
                 nodetool_cmd = 'nodetool -h localhost -p 7199 snapshot %s ' % custom_key
+                skip_key = ','.join(skip_key)
             else:
                 nodetool_cmd = 'nodetool -h localhost -p 7199 snapshot'
         sudo(nodetool_cmd)
-        snapshot_dirs = sudo("find %s/  -name 'snapshots' " % db_path)
+        
+        #skip snapshots of skipped_keyspaces if already present
+        if skip_key:
+            snapshot_dirs = sudo("find %s/  -name 'snapshots' | egrep -v $(echo %s | sed -r 's/,/|/g')" %(db_path,skip_key))
+        else:
+            snapshot_dirs = sudo("find %s/  -name 'snapshots' " % db_path)
         snapshot_dirs = snapshot_dirs.split('\r\n')
         #get relative path to cassandra from db_path 
         path_to_cassandra = re.search('.*/cassandra/',db_path).group(0)
