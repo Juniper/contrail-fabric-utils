585a2200555c949d09de33ca2ab7bead89b60140
Pavana <pavanap@juniper.net>
2016-05-17 13:44:34 +0530

    Adding step to install the compatible version of paramiko - 2.22.x branch
    Bug #1582531
    
    Change-Id: I5c5bc71786604b9ffe32431ffec1d8dd905d0f5f

--- fabfile/tasks/tester.py
+++ fabfile/tasks/tester.py
@@ -366,6 +366,7 @@ def setup_test_env():
                 # On centos, junos-eznc install requires devel pkgs of libxml2 and libxslt
                 if detect_ostype() in ['redhat', 'centos', 'centoslinux']:
                     pkg_install(['libxslt-devel', 'libxml2-devel'], disablerepo=False)
+                sudo('pip install paramiko==1.17.0')
                 sudo('pip install junos-eznc==1.2.2')
 
         for host_string in env.roledefs['compute']:
