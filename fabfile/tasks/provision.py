32f77a777dcd5958352af486fd1b215f14598eec
Raja Sivaramakrishnan <raja@juniper.net>
2016-06-23 15:23:34 -0700

    Merge following 3.0 commit into master.
    commit 4c0421eac744db57f69ac06a9e8ad51fe39519cf
    Author: Raja Sivaramakrishnan <raja@juniper.net>
    Date:   Thu May 19 00:08:42 2016 -0700
    
        Bring up all interfaces after creating VFs (so that bond interface can
        come up after VFs are added).
    
        Change-Id: I4b299a8578483907f3cd2eef706f4d09ecc8fdf4
        Partial-Bug: 1573255
    
    Change-Id: I3877c8f9b97e02cf765cd0cbcf3bde9ef859395d

--- fabfile/tasks/provision.py
+++ fabfile/tasks/provision.py
@@ -2625,7 +2625,9 @@ def setup_sriov_interfaces(host_string, host_config, bondinfo):
     '''
     def enable_vf(iface, vf):
         with settings(host_string=host_string):
-            str = 'echo %s > /sys/class/net/%s/device/sriov_numvfs' % (vf,
+            # Keep this command consistent with 
+            # contrail_provisioning/common/base.py
+            str = 'echo %s > /sys/class/net/%s/device/sriov_numvfs; sleep 2; ifup -a' % (vf,
                                                                        iface)
             sudo(str)
 
