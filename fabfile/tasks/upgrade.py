3837f79bcd24ea17b95d271c5abe8281f564f1cf
Hari Prasad Killi <haripk@juniper.net>
2017-03-03 10:16:46 +0530

    upgrade : remove check for R3.0.2.0
    Change-Id: Ie233c91415deb8bbd250d4b8bb98824ea0818db3
    closes-bug: #1664422

--- fabfile/tasks/upgrade.py
+++ fabfile/tasks/upgrade.py
@@ -214,12 +214,7 @@ def upgrade_compute_node(from_rel, pkg, *args, **kwargs):
                 if (getattr(env, 'interface_rename', True) and
                     detect_ostype() not in ['ubuntu', 'redhat']):
                     pkgs.append('contrail-interface-name')
-                if LooseVersion(from_rel) <= LooseVersion('3.0.2.0'):
-                    dist, version, extra = get_linux_distro()
-                    if version == '14.04' and 'contrail-vrouter-3.13.0-40-generic' in pkgs:
-                       pkgs.remove('contrail-vrouter-3.13.0-40-generic')
-                       pkgs.append('contrail-vrouter-3.13.0-85-generic')
-                elif LooseVersion(from_rel) <= LooseVersion('3.2.0.0'):
+                if LooseVersion(from_rel) <= LooseVersion('3.2.0.0'):
                     dist, version, extra = get_linux_distro()
                     if version == '14.04':
                        if 'contrail-vrouter-3.13.0-40-generic' in pkgs:
