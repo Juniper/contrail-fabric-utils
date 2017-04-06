3c8c0998f23a8776c0c52a3cb49dc67daa08f46b
Raj Reddy <rajreddy@juniper.net>
2017-04-05 17:16:08 -0700

    make ls command run in warn_only flag so it doesn't exit
    the whole issue script exits when sudo ls command returns a failure,
    this fix will prevent that
    
    Closes-Bug: #1680271
    
    Change-Id: I295c73639f8cc8411fadcf8fadfb55e927114e45
    (cherry picked from commit d2127690bd85d862714f1ed380400407083d3f42)

--- fabfile/tasks/issu_process.py
+++ fabfile/tasks/issu_process.py
@@ -41,7 +41,8 @@ def issu_contrail_switch_collector_in_compute_node(*args):
             for i in range(0, max_collectors):
                 collector_list += "%s:8086 " %(hstr_to_ip(get_control_host_string(env.roledefs['collector'][host_num])))
                 host_num = (host_num + 1) % num_collectors
-            file_list = sudo('ls /etc/contrail/contrail-tor-agent*')
+            with settings(warn_only=True):
+                file_list = sudo('ls /etc/contrail/contrail-tor-agent*')
             if file_list.succeeded:
                 file_list = file_list.split()
             else:
