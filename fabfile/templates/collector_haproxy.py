42c6ebc2d41d6b4d80e10ecad20105739c1e2ed9
Amudha <amudha@juniper.net>
2017-05-31 20:37:15 -0700

    Fix haproxy syntax change in collector_haproxy.py
    Change-Id: Ic96635eeb3f8b924a902a5cbec49bff25333af8a
    Closes-Bug: #1694858

--- fabfile/templates/collector_haproxy.py
+++ fabfile/templates/collector_haproxy.py
@@ -1,13 +1,15 @@
 import string
 
 template = string.Template("""#contrail-collector-marker-start
-listen contrail-collector-stats :5938
+listen contrail-collector-stats
+   bind :5938
    mode http
    stats enable
    stats uri /
    stats auth $__contrail_hap_user__:$__contrail_hap_passwd__
 
-frontend  contrail-analytics-api *:8081
+frontend  contrail-analytics-api
+    bind *:8081
     default_backend    contrail-analytics-api
 
 backend contrail-analytics-api
