b58d9674b830506cc009c4e02bd48f8b94614ec6
Senthilnathan Murugappan <msenthil@juniper.net>
2017-06-16 12:13:34 -0700

    Adding python-cffi as dependency which is required for junos-eznc
    Change-Id: I6317121d66b8d6d36dff36bcc4f4ec25774f9bad
    Closes-Bug:#1698439

--- fabfile/tasks/tester.py
+++ fabfile/tasks/tester.py
@@ -366,6 +366,7 @@ def setup_test_env():
                 # On centos, junos-eznc install requires devel pkgs of libxml2 and libxslt
                 if detect_ostype() in ['redhat', 'centos', 'centoslinux']:
                     pkg_install(['libxslt-devel', 'libxml2-devel'], disablerepo=False)
+                sudo('pip install pycparser==2.17')
                 sudo('pip install paramiko==1.17.0')
                 sudo('pip install junos-eznc==1.2.2')
 
