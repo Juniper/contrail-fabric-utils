c13fa6b155a7a36bb89802a353edbb8e6e7ac176
Senthilnathan Murugappan <msenthil@juniper.net>
2017-06-21 19:47:37 -0700

    Fix pkgs list reqd for junos-eznc
    Refer https://stackoverflow.com/questions/22073516/failed-to-install-python-cryptography-package-with-pip-and-setup-py
    for more info. Closes-Bug:#1698439
    
    Change-Id: If02844c266b8311465c1b2ba0018cc8c40f4ac24

--- fabfile/tasks/tester.py
+++ fabfile/tasks/tester.py
@@ -365,8 +365,8 @@ def setup_test_env():
 
                 # On centos, junos-eznc install requires devel pkgs of libxml2 and libxslt
                 if detect_ostype() in ['redhat', 'centos', 'centoslinux']:
-                    pkg_install(['libxslt-devel', 'libxml2-devel'], disablerepo=False)
-                sudo('pip install pycparser==2.17')
+                    pkg_install(['libxslt-devel', 'libxml2-devel', 'libffi-devel',
+                                 'gcc', 'python-devel', 'openssl-devel'], disablerepo=False)
                 sudo('pip install paramiko==1.17.0')
                 sudo('pip install junos-eznc==1.2.2')
 
