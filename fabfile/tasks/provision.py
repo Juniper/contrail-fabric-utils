10535d48cf0901bde5d96901cd68b425e04bde6c
Ignatious Johnson Christopher <ijohnson@juniper.net>
2018-02-23 22:25:56 -0800

    Create ssl certs for discovery server
    - ssl termination using haproxy
    - passing the certs as args to the setup-vnc-<role>
    entrypoints
    
    Change-Id: Ic06660e7243ce2d7c8290a402968366fd737fda1
    Closes-Bug: 1728676

--- fabfile/tasks/provision.py
+++ fabfile/tasks/provision.py
@@ -36,7 +36,8 @@ from fabfile.tasks.ssl import (setup_keystone_ssl_certs_node,
         setup_apiserver_ssl_certs_node, copy_keystone_ssl_certs_to_node,
         copy_apiserver_ssl_certs_to_node, copy_vnc_api_lib_ini_to_node,
         copy_certs_for_neutron_node, copy_certs_for_heat,
-        use_keystone_ssl_certs_in_node)
+        use_keystone_ssl_certs_in_node, setup_discovery_ssl_certs_node,
+        copy_discovery_ssl_certs_to_node)
 
 
 FAB_UTILS_DIR = '/opt/contrail/utils/fabfile/utils/'
@@ -59,6 +60,8 @@ def setup_cfgm():
     """Provisions config services in all nodes defined in cfgm role."""
     if env.roledefs['cfgm']:
         execute("setup_cfgm_node", env.host_string)
+    if discovery_ssl_enabled():
+        execute("copy_discovery_ssl_certs_to_node", *env.roledefs['database'])
 
 @roles('cfgm')
 @task
@@ -132,7 +135,7 @@ $__contrail_api_frontend__
     default_backend    contrail-api-backend
     timeout client 3m
 
-frontend  contrail-discovery *:5998
+$__contrail_disc_frontend__
     default_backend    contrail-discovery-backend
 
 backend quantum-server-backend
@@ -154,6 +157,7 @@ $__contrail_api_backend_servers__
 backend contrail-discovery-backend
     option nolinger
     balance     roundrobin
+$__contrail_disc_ssl_forwarding__
 $__contrail_disc_backend_servers__
     #server  10.84.14.2 10.84.14.2:9110 check
     #server  10.84.14.2 10.84.14.2:9111 check
@@ -173,6 +177,8 @@ $__rabbitmq_config__
     api_ssl_forwarding = ''
     api_server_lines = ''
     disc_listen_port = 9110
+    disc_frontend = 'frontend  contrail-discovery *:5998'
+    disc_ssl_forwarding = ''
     disc_server_lines = ''
     tor_agent_ha_config = ''
     rabbitmq_config = """
@@ -232,6 +238,14 @@ listen  rabbitmq 0.0.0.0:5673
     http-request set-header X-Forwarded-Port %[dst_port]
     http-request add-header X-Forwarded-Proto https if { ssl_fc }"""
 
+    if discovery_ssl_enabled():
+        disc_frontend = """frontend  contrail-api
+    bind *:5998 ssl crt /etc/contrail/ssl/certs/discoverycertbundle.pem"""
+        disc_ssl_forwarding = """    option forwardfor
+    http-request set-header X-Forwarded-Port %[dst_port]
+    http-request add-header X-Forwarded-Proto https if { ssl_fc }"""
+
+
 
     for host_string in env.roledefs['cfgm']:
         haproxy_config = template.safe_substitute({
@@ -241,6 +255,8 @@ listen  rabbitmq 0.0.0.0:5673
             '__contrail_api_frontend__': api_frontend,
             '__contrail_api_ssl_forwarding__': api_ssl_forwarding,
             '__contrail_api_backend_servers__': api_server_lines,
+            '__contrail_disc_frontend__': disc_frontend,
+            '__contrail_disc_ssl_forwarding__': disc_ssl_forwarding,
             '__contrail_disc_backend_servers__': disc_server_lines,
             '__contrail_hap_user__': 'haproxy',
             '__contrail_hap_passwd__': get_haproxy_token('cfgm'),
@@ -627,6 +643,8 @@ def setup_cfgm_node(*args):
                 execute("copy_keystone_ssl_certs_to_node", host_string)
             if apiserver_ssl_enabled():
                 execute("copy_certs_for_neutron_node", host_string)
+            if discovery_ssl_enabled():
+                execute("setup_discovery_ssl_certs_node", host_string)
             enable_haproxy()
     nworkers = 1
     fixup_restart_haproxy_in_all_cfgm(nworkers)
@@ -1372,6 +1390,8 @@ def setup_collector_node(*args):
                 execute("copy_keystone_ssl_certs_to_node", host_string)
             if apiserver_ssl_enabled():
                 execute("copy_apiserver_ssl_certs_to_node", host_string)
+            if discovery_ssl_enabled():
+                execute("copy_discovery_ssl_certs_to_node", host_string)
             if detect_ostype() == 'ubuntu':
                 with settings(warn_only=True):
                     sudo('rm /etc/init/supervisor-analytics.override')
@@ -1686,6 +1706,8 @@ def setup_control_node(*args):
                 with settings(warn_only=True):
                     sudo('rm /etc/init/supervisor-control.override')
                     sudo('rm /etc/init/supervisor-dns.override')
+            if discovery_ssl_enabled():
+                execute("copy_discovery_ssl_certs_to_node", host_string)
             with cd(INSTALLER_DIR):
                 sudo(cmd)
                 if detect_ostype() in ['centos', 'redhat', 'fedora', 'centoslinux']:
@@ -1805,6 +1827,8 @@ def setup_only_vrouter_node(manage_nova_compute='yes', configure_nova='yes', *ar
             if apiserver_ssl_enabled():
                 execute("copy_apiserver_ssl_certs_to_node", host_string)
                 execute("copy_vnc_api_lib_ini_to_node", host_string)
+            if discovery_ssl_enabled():
+                execute("copy_discovery_ssl_certs_to_node", host_string)
 
             enable_haproxy()
         haproxy = get_haproxy_opt()
